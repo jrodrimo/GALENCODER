654b94929f2b48b87d11cb20af0b8e2c43d2cc1a
jrodrimo <jrodrimo@users.noreply.github.com>
2022-12-22 01:15:34 +0100

    Delete download_file_from_street.m

--- download_file_from_street.m
+++
@@ -1,7 +0,0 @@
-dir=pwd;
-lat=mean([max(data_loaded.lat) min(data_loaded.lat)]);
-lon=mean([max(data_loaded.lon) min(data_loaded.lon)]);
-pageWeb=strcat('''','https://www.openstreetmap.org/export#map=18/',...
-    num2str(lat),'/',num2str(lon),'''');
-eval(strcat('web(',pageWeb,',','''','-browser','''',')'));
-
